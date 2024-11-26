86e96a54f8d87a18db22d6ab3691a62cff245565
maxitonia20 <max66604@gmail.com>
2024-11-26 15:11:38 +0100

    search functionality

--- src/component/Navbar_components/Searchbar.jsx
+++ src/component/Navbar_components/Searchbar.jsx
@@ -3,28 +3,55 @@
 import React from 'react'
 import { searchvideos } from '@/util/fetchData'
 import { useState } from 'react'
-
+import image from 'next/image'
 const Searchbar = () => {
 
-  const [ count, setCount ] = useState(0);
-  function update(){
-    setCount(count+1)
-  }
+  const [ loading, setLoading ] = useState(false);
+const [result, setResult] = useState([])
+const [ask, setAsk] =useState ("")
+
+const handleSearch = async (e) =>{
+  e.preventDefault(),
+  setLoading(true)
+
+  const date = await searchvideos(`search/?query=${ask}`)
+  console.log (date)
+  setResult(date.videos)
+  setLoading (false)
+}
+ // function update(){
+    //setCount(count+1)
+
 
   return (
     <div>
-      
-      <form>
-        <input type="text" placeholder='search..'/>
-        <button>
-          search
-        </button>
-        </form>
-        <h1> {count} </h1>
-        <button onClick={update} > update count </button>
-        
-    </div>
-  )
+      <form onSubmit={handleSearch}>
+      <input type="text" placeholder='Search..'
+      value={ask}
+      onChange={(e) => setAsk(e.target.value
+      )}/>
+      <button>
+     search
+   </button>
+   </form>
+
+   {
+  loading ? <p>loading...</p> : null
+  }
+
+{
+  result.map((eachItems, index,array) => ( 
+<div key={index}> <h1> {eachItems.title} </h1>
+ <h1> {eachItems.number_of_vew} </h1>
+ <h1> {eachItems.video_length} </h1>
+ <img src= {eachItems.thumbnails[0].url}  alt="image" />
+</div>
+))
 }
+    </div>
+ )
+}
+  
+
 
 export default Searchbar
--- src/util/fetchData.js
+++ src/util/fetchData.js
@@ -1,13 +1,13 @@
 const BASE_URL = 'https://youtube-v2.p.rapidapi.com'
 
- export const searchvideos = (query ) => {
-    const response = fetch(`${BASE_URL}/${query}`,{
+ export const searchvideos = async (query ) => {
+    const response = await fetch(`${BASE_URL}/${query}`,{
         method: "GET",
         headers: {
             'x-rapidapi-key': 'f6e6c5e846mshecff228758e72fep1b8b56jsn9ef040e3cdc3',
 		'x-rapidapi-host': 'youtube-v2.p.rapidapi.com'
         }
     })
-    const result = response.json();
+    const result = await response.json();
     return result;
 }
